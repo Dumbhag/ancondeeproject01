054ed4e554d9e7714afe88e7107ae02f4fc3e043
Dumbhag <s6519410057@sau.ac.th>
2023-08-18 12:00:20 +0700

    code v.1: create 3. file

---
+++ yaro03.py
@@ -0,0 +1,12 @@
+print('AAA'); print("BBB")
+print("CCC")
+print("DDD\nEEE\nFFF")
+print("DDD\tEEE\tFFF")
+print('Welcome "SAU"')
+print("Welcome \"SAU\"")
+print('Welcome "TOT"')
+print('ABC\\DEF')
+print('''
+      kas
+      asd
+      asd''')
